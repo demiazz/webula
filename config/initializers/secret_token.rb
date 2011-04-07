d1a64fdc1b1b50dd2d85b5d395642034eef54c7b
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-07 21:08:58 +0400

    Initialized new RoR application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webula::Application.config.secret_token = '78d8338cf5ab11c5b80e50522a4c6a7916a2a0806db13d6f3a25801938a25176ce9ee75b431dffa93bc33f260c35141f1a440e18f7d994836c69f0f27b399254'
