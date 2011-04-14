5fc8d95506c0ec5eddd8dfd618e0efc8c8f8bb32
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-14 22:37:08 +0400

    Configured Devise Admin model.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,13 +1,29 @@
 class Admin
   include Mongoid::Document
-
-  # Devise Modules
-  devise :database_authenticatable, 
-         :recoverable, 
-         :trackable, 
-         :validatable,
-         :confirmable,
-         :lockable,
-         :timeoutable
+  
+  devise :database_authenticatable,
+  		 # ==> Modules
+  		 :recoverable,
+  		 :trackable,
+  		 :validatable,
+  		 :lockable,
+  		 :timeoutable,
+  		 # ==> Configuration for any authentication mechanism
+  		 :authentication_keys => [ :email ],
+  		 :case_insensitive_keys => [ :email ],
+  		 # ==> Configuration for :database_authenticatable
+  		 :stretches => 10,
+  		 # ==> Configuration for :validatable
+  		 :password_length => 8..32,
+  		 # ==> Configuration for :timeoutable
+  		 :timeout_in => 5.minutes,
+  		 # ==> Configuration for :lockable
+  		 :lock_strategy => :failed_attempts,
+  		 :unlock_keys => [ :email ],
+  		 :unlock_strategy => :both,
+  		 :maximum_attempts => 5,
+	  	 :unlock_in => 7.days,
+		 # ==> Configuration for :recoverable
+  		 :reset_password_keys => [ :email ]
 
 end
