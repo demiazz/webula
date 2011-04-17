a8f143292a0011203e35a9669308530f36dd64bb
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-17 23:39:16 +0400

    Added main/organization/contacts/avatar_save actions to ProfileManager controller.

--- app/controllers/profile_master_controller.rb
+++ app/controllers/profile_master_controller.rb
@@ -9,6 +9,22 @@ class ProfileMasterController < ApplicationController
   end
 
   def main_save
+    @user_profile = current_user.user_profile
+
+    @user_profile.first_name = params[:first_name]
+    @user_profile.last_name = params[:last_name]
+    @user_profile.birthday = Date.new(params[:birthday][:year].to_i, params[:birthday][:month].to_i, params[:birthday][:day].to_i)
+    @user_profile.country = params[:country]
+    @user_profile.state = params[:state]
+    @user_profile.city = params[:city]
+
+    respond_to do |format|
+      if @user_profile.save
+        format.html { redirect_to :controller => "profile_master", :action => "organization_edit" }
+      else
+        format.html { render :action => "main_edit" }
+      end
+    end
   end
 
   def organization_edit
@@ -18,24 +34,73 @@ class ProfileMasterController < ApplicationController
   end
 
   def organization_save
+    @user_profile = current_user.user_profile
+
+    @user_profile.org_name = params[:org_name]
+    @user_profile.org_country = params[:org_country]
+    @user_profile.org_state = params[:org_state]
+    @user_profile.org_city = params[:org_city]
+    @user_profile.org_unit = params[:org_unit]
+    @user_profile.org_position = params[:org_position]
+
+    respond_to do |format|
+      if @user_profile.save
+        format.html { redirect_to :controller => "profile_master", :action => "contacts_edit" }
+      else
+        format.html { render :action => "organization_edit" }
+      end
+    end
   end
 
   def contacts_edit
     respond_to do |format|
-      format.html #organization_edit.html.erb
+      format.html #contacts_edit.html.erb
     end
   end
 
   def contacts_save
+    @user_profile = current_user.user_profile
+
+    @user_profile.home_phone = params[:home_phone]
+    @user_profile.work_phone = params[:work_phone]
+    @user_profile.mobile_phone = params[:mobile_phone]
+    @user_profile.mail = params[:mail]
+    @user_profile.icq = params[:icq]
+    @user_profile.xmpp = params[:xmpp]
+    @user_profile.twitter = params[:twitter]
+
+    respond_to do |format|
+      if @user_profile.save
+        format.html { redirect_to :controller => "profile_master", :action => "avatar_edit" }
+      else
+        format.html { render :action => "contacts_edit" }
+      end
+    end
   end
 
   def avatar_edit
     respond_to do |format|
-      format.html #organization_edit.html.erb
+      format.html #avatar_edit.html.erb
     end
   end
 
   def avatar_save
+    @user_profile = current_user.user_profile
+
+    uploaded_io = params[:avatar]
+    File.open(Rails.root.join('public/images', 'avatars', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+
+    @user_profile.avatar = "images/avatars/" + params[:avatar].original_filename
+  
+    respond_to do |format|
+      if @user_profile.save
+        format.html
+      else
+        format.html { render :action => "avatar_edit" }
+      end
+    end
   end
 
   def finish
