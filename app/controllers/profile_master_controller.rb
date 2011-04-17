124ba753494f4d8c3f7020e94cf88551af7db149
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-17 23:33:44 +0400

    Added main/organization/contacts/avatar_edit actions to ProfileMaster controller.

--- app/controllers/profile_master_controller.rb
+++ app/controllers/profile_master_controller.rb
@@ -3,24 +3,36 @@ class ProfileMasterController < ApplicationController
   before_filter :authenticate_user!
 
   def main_edit
+    respond_to do |format|
+      format.html #main_edit.html.erb
+    end
   end
 
   def main_save
   end
 
   def organization_edit
+    respond_to do |format|
+      format.html #organization_edit.html.erb
+    end
   end
 
   def organization_save
   end
 
   def contacts_edit
+    respond_to do |format|
+      format.html #organization_edit.html.erb
+    end
   end
 
   def contacts_save
   end
 
   def avatar_edit
+    respond_to do |format|
+      format.html #organization_edit.html.erb
+    end
   end
 
   def avatar_save
