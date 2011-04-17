a031e7a6e8eb7f6450c46a672075327dbc3b96f6
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-18 03:59:38 +0400

    Added filter in ApplicationController for redirect on ProfileMaster controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+
+  before_filter :authenticate_user!
+  before_filter :profile
+
+  private
+
+    def profile
+      if user_signed_in?
+        if current_user.user_profile.new_profile
+          redirect_to :controller => "profile_master", :action => "main_edit"
+        end
+      end
+    end
+
 end
