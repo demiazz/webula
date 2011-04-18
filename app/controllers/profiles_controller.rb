6d8d3f1ebb91f2ba8e141300db7cf98a95ea0b6f
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-19 02:36:55 +0400

    Written actions for Profiles controller.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,21 @@
 class ProfilesController < ApplicationController
+
   def index
+  	@user = current_user
+  	@user_profile = current_user.user_profile
+
+  	respond_to do |format|
+  	  format.html #index.html.erb
+  	end
   end
 
   def show
+  	@user = User.first(conditions: { username: params[:username] })
+  	@user_profile = @user.user_profile
+
+  	respond_to do |format|
+  	  format.html #show.html.erb
+  	end
   end
 
 end
