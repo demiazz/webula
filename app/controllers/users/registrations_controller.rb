3db6046294ee256164c3644917ad4174300d2aec
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-20 08:00:18 +0400

    Regenerated Users::RegistrationsController.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,21 +1,2 @@
-# -*- coding: utf-8 -*-
-
-#
-# Webula SN
-#
-# Замена для Devise::RegistrationsController для
-# модели User.
-#
-# Copyright (c) 2011, Alexey Plutalov
-# License: GPL
-#
-
-# Класс замена для Devise::RegistrationsController.
-class Users::RegistrationsController < Devise::RegistrationsController
-
-  # Изменяет перенаправление после обновления профиля пользователя.
-  def after_update_path_for(resource)
-    url_for :action => "edit"
-  end
-
+class Users::RegistrationsController < ApplicationController
 end
