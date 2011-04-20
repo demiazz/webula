d06db660121b89ff9fda89de8a59f2319fe63620
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-20 06:09:17 +0400

    Refactoring ProfilesController.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,27 @@
+# -*- coding: utf-8 -*-
+
+#
+# Webula SN
+#
+# Контроллер отображения профиля пользователя.
+#
+# Copyright (c) 2011, Alexey Plutalov
+# License: GPL
+#
+
+# Контроллер отображения профиля пользователя.
 class ProfilesController < ApplicationController
 
+  # Отображает профиль текущего пользователя
   def index
-  	@user = current_user
-  	@user_profile = current_user.user_profile
-
   	respond_to do |format|
   	  format.html #index.html.erb
   	end
   end
 
+  # Отображает профиль указанного профиля.
+  #
+  # Для поиска пользователя используется params[:username].
   def show
   	@user = User.first(conditions: { username: params[:username] })
   	@user_profile = @user.user_profile
