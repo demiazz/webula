9888b177e64e099f5d06d84caa1bedcd869bc47d
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-20 05:42:30 +0400

    Refactoring ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,48 @@
+# -*- coding: utf-8 -*-
+
+#
+# Webula SN
+#
+# Базовый класс контроллера.
+#
+# Copyright (c) 2011, Alexey Plutalov
+# License: GPL
+#
+
+# Базовый класс контроллера
 class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
+  # Требует аутентификации пользователя
   before_filter :authenticate_user!
+  # Переключатель мастера профиля
   before_filter :profile
+  # Загрузка пользовательской информации
   before_filter :personalize
 
   private
-
+    
+    # Переключатель мастера профиля
+    # 
+    # Включает/выключает мастер профиля для пользователя.
+    # * Если пользователь имеет профиль, то мастер недоступен.
+    # * Если пользователь не имеет профиля, то мастер доступен.
     def profile
-      if user_signed_in?
-        if current_user.user_profile.new_profile
-          redirect_to :controller => "profile_master", :action => "main_edit"
-        end
+      if user_signed_in? and current_user.user_profile.new_profile
+        redirect_to :controller => "profile_master", :action => "main_edit"
       end
     end
 
+    # Загрузка пользовательской информации
+    #
+    # При каждом запросе производит загрузку аккаунта и профиля
+    # пользователя.
+    # * Если пользователь авторизован, происходит загрузка.
+    # * Если страница для текущего пользователя, то используется
+    #   current_user.
+    # * Если страница не текущего пользователя, то загружается
+    #   информация пользователя с username из params[:username].
     def personalize
       if user_signed_in?
         if params[:username].nil?
