52f7fc1eb8d8ed50d15e8f669a9b2da04ce8ae83
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-20 06:10:14 +0400

    Tabs replaced by spaces in ProfilesController.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,21 +14,21 @@ class ProfilesController < ApplicationController
 
   # Отображает профиль текущего пользователя
   def index
-  	respond_to do |format|
-  	  format.html #index.html.erb
-  	end
+    respond_to do |format|
+      format.html #index.html.erb
+    end
   end
 
   # Отображает профиль указанного профиля.
   #
   # Для поиска пользователя используется params[:username].
   def show
-  	@user = User.first(conditions: { username: params[:username] })
-  	@user_profile = @user.user_profile
+    @user = User.first(conditions: { username: params[:username] })
+    @user_profile = @user.user_profile
 
-  	respond_to do |format|
-  	  format.html #show.html.erb
-  	end
+    respond_to do |format|
+      format.html #show.html.erb
+    end
   end
 
 end
