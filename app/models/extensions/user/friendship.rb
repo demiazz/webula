e50a7d8eef44701de61792a318da752e73fe7a70
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-25 05:07:59 +0400

    Removed unused files.

--- app/models/extensions/user/friendship.rb
+++
@@ -1,7 +0,0 @@
-module Extensions::User::Friendship
-
-  # Связи
-  embeds_one :friendship_stat # Статистика пользовательских отношений
-  has_and_belongs_to_many :friends, :class_name => "User"
-
-end
