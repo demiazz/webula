8c227d557381065c28868bfec410dba6c962005a
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-26 15:27:26 +0400

    Written MicroblogController#global_feed.

--- app/controllers/microblog_controller.rb
+++ app/controllers/microblog_controller.rb
@@ -1,5 +1,25 @@
 class MicroblogController < ApplicationController
+
+  # Глобальная лента
   def global_feed
+    # Получение всех постов с сортировкой по дате создания
+    @posts = MicroblogPost.all.desc(:created_at).to_a
+    # Генерация списка id авторов постов
+    author_ids = Array.new
+    @posts.each do |post|
+      author_ids << post.author_id
+    end
+    # Получение авторов постов по id
+    authors = Hash.new
+    User.where(:_id.in => author_ids.uniq).
+         only(:id, :username, "user_profile.first_name", "user_profile.last_name")
+         .each do |author|
+      authors[author.id] = author
+    end
+    # Присваивание авторов постам по author_id
+    @posts.each do |post|
+      post.author = authors[post.author_id]
+    end
   end
 
   def local_feed
