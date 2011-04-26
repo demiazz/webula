eaa536fdde1bb5faad40c1a64939490f32d224b7
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-26 17:47:25 +0400

    Added relation between Microblog and User models. Added fields for Microblog model.

--- app/models/microblog.rb
+++ app/models/microblog.rb
@@ -1,3 +1,16 @@
 class Microblog
   include Mongoid::Document
+
+  # Пользователи, которых читает пользователь по id
+  field :following_ids, :type => Array, :default => []
+  # Читатели пользователя по id
+  field :follower_ids, :type => Array, :default => []
+  # Число пользователей, которых пользователь читает
+  field :followings_count, :type => Integer, :default => 0
+  # Число читателей пользователя
+  field :followers_count, :type => Integer, :default => 0
+
+  # Имеет одного хозяина
+  belongs_to :owner, :class_name => "User", :inverse_of => :microblog
+
 end
