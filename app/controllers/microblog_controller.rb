e519f4387e7b92ee5a7838610f1e7a3618687778
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-27 20:48:15 +0400

    Moved default page from MicroblogController to routes.

--- app/controllers/microblog_controller.rb
+++ app/controllers/microblog_controller.rb
@@ -12,9 +12,6 @@ class MicroblogController < ApplicationController
 
   # Глобальная лента
   def global_feed
-    if params[:page].nil?
-      params[:page] = 1
-    end
     if @personal
       @new_post = MicroblogPost.new
     end
@@ -24,9 +21,6 @@ class MicroblogController < ApplicationController
   end
 
   def local_feed
-    if params[:page].nil?
-      params[:page] = 1
-    end
     if @personal
       @new_post = MicroblogPost.new
     end
@@ -46,9 +40,6 @@ class MicroblogController < ApplicationController
   #
   # Если пользователь текущий: генерирует новый MicroblogPost объект для формы.
   def personal_feed
-    if params[:page].nil?
-      params[:page] = 1
-    end
     if @personal
       @microblog = Microblog.where(:owner_id => @user.id).only(:posts_count).first
       @new_post = MicroblogPost.new
@@ -67,9 +58,6 @@ class MicroblogController < ApplicationController
   end
 
   def followings_feed
-    if params[:page].nil?
-      params[:page] = 1
-    end
     # Получение постов пользователей, на кого подписан пользователь
     @posts_count, @posts = get_posts(MicroblogPost.
                                        where(:author_id.in => @user.microblog.following_ids).
@@ -78,9 +66,6 @@ class MicroblogController < ApplicationController
   end
 
   def followers_feed
-    if params[:page].nil?
-      params[:page] = 1
-    end
     # Получение постов, подписчиков пользователей
     @posts_count, @posts = get_posts(MicroblogPost.
                                        where(:author_id.in => @user.microblog.follower_ids).
@@ -91,9 +76,6 @@ class MicroblogController < ApplicationController
   # Кого читает пользователь
   def followings
     unless @microblog.followings_count == 0
-      if params[:page].nil?
-        params[:page] = 1
-      end
       @followings = User.where(:_id.in => @microblog.following_ids).
                          only(:id, :username, "user_profile.first_name", 
                               "user_profile.last_name", "user_profile.avatar",
@@ -107,9 +89,6 @@ class MicroblogController < ApplicationController
   # Читатели микроблога пользователя
   def followers
     unless @microblog.followers_count == 0
-      if params[:page].nil?
-        params[:page] = 1
-      end
       @followers = User.where(:_id.in => @microblog.follower_ids).
                               only(:id, :username, "user_profile.first_name", 
                               "user_profile.last_name", "user_profile.avatar",
