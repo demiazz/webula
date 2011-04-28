e13e28cb557ccfa12330feaab0f9b72680ac859c
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-28 12:06:19 +0400

    Added scopes to MicroblogPost model.

--- app/models/microblog_post.rb
+++ app/models/microblog_post.rb
@@ -6,4 +6,9 @@ class MicroblogPost
 
   belongs_to :author, :class_name => "User", :inverse_of => :microblog_posts
 
+  # Scopes
+  scope :all_posts, all.desc(:created_at)
+  scope :author_ids, ->(ids) { where(:author_id.in => ids).desc(:created_at) }
+  scope :author_id, ->(id) { where(:author_id => id).desc(:created_at) }
+
 end
