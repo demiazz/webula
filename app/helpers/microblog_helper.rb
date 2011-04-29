de492f74f764db1a6f419fbdb87a18697ab9d69b
Alexey Plutalov <demiazz.py@gmail.com>
2011-04-29 20:21:02 +0400

    Added helpers for generate links to home microblog.

--- app/helpers/microblog_helper.rb
+++ app/helpers/microblog_helper.rb
@@ -1,2 +1,15 @@
 module MicroblogHelper
+
+  def to_microblog_home(name="Microblog", html_options = {})
+    link_to name, url_for(:controller => "microblog",
+                          :action => "local_feed",
+                          :username => nil), html_options
+  end
+
+  def to_microblog_user_home(user, name="Microblog", html_options = {})
+    link_to name, url_for(:controller => "microblog",
+                          :action => "personal_feed",
+                          :username => user.username), html_options
+  end
+
 end
