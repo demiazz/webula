14e25a0d4f85d89c198411d25aabe9ed872d51fc
Alexey Plutalov <demiazz.py@gmail.com>
2011-05-03 01:07:29 +0400

    Added new helpers. Fixed bugs.

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -15,8 +15,7 @@ module FriendshipHelper
                                "selected" :
                                html_options[:class].concat(" selected")
     end
-    username = user.nil? ? nil : user.username
-    link_to name, friendship__friends_path(:username => username), html_options
+    link_to name, friendship__friends_path(:username => user.username), html_options
   end
 
   def to_requests_to(name="Requests for you", html_options={})
@@ -35,6 +34,56 @@ module FriendshipHelper
                                html_options[:class].concat(" selected")
     end
     link_to name, friendship__requests_from_path, html_options
+  end
+
+  def to_mutual_friends(user, name="Mutual friends", html_options={})
+    if action_name == "mutual_friends"
+      html_options[:class] = html_options[:class].nil? ?
+                               "selected" :
+                               html_options[:class].concat(" selected")
+    end
+    link_to name, friendship__mutual_friends_path(:username => user.username), html_options
+  end
+
+  def to_not_mutual_friends(user, name="Not mutual friends", html_options={})
+    if action_name == "not_mutual_friends"
+      html_options[:class] = html_options[:class].nil? ?
+                               "selected" :
+                               html_options[:class].concat(" selected")
+    end
+    link_to name, friendship__not_mutual_friends_path(:username => user.username), html_options
+  end
+
+  def to_add_friend(user, name="Add to friends", html_options={})
+    html_options[:class] = html_options[:class].nil? ?
+                             "constructive" :
+                             html_options[:class].concat(" destructive")
+    html_options[:method] = :put
+    link_to name, friendship__add_friend_path(:friend => user.username), html_options
+  end
+
+  def to_confirm_friend(user, name="Confirm request", html_options={})
+    html_options[:class] = html_options[:class].nil? ?
+                             "constructive" :
+                             html_options[:class].concat(" destructive")
+    html_options[:method] = :put
+    link_to name, friendship__confirm_friend_path(:friend => user.username), html_options
+  end
+
+  def to_refuse_friend(user, name="Refuse request", html_options={})
+    html_options[:class] = html_options[:class].nil? ?
+                             "destructive" :
+                             html_options[:class].concat(" destructive")
+    html_options[:method] = :put
+    link_to name, friendship__refuse_friend_path(:friend => user.username), html_options
+  end
+
+  def to_remove_friend(user, name="Remove from friends", html_options={})
+    html_options[:class] = html_options[:class].nil? ?
+                             "destructive" :
+                             html_options[:class].concat(" destructive")
+    html_options[:method] = :put
+    link_to name, friendship__remove_friend_path(:friend => user.username), html_options
   end
 
   def to_friends_home(name="Friends", html_options={})
