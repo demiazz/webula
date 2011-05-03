cd0923db5b69b01712977bc130e8286bf6c18c19
Alexey Plutalov <demiazz.py@gmail.com>
2011-05-03 14:27:12 +0400

    Added inbox/outbox actions.

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,2 +1,42 @@
+# -*- coding: utf-8 -*-
+
+=begin
+
+================================================================================
+
+Class: MicroblogController
+
+Description:
+  Контроллер почтовых сообщений.
+
+================================================================================
+
+Actions:
+  * inbox
+  * outbox
+
+================================================================================
+
+Copyright (c) 2011, Alexey Plutalov <demiazz.py@gmail.com>
+
+================================================================================
+
+=end
+
 class MailController < ApplicationController
+
+  def inbox
+    @messages_count = Message.where(:recipient_id => @user.id).count
+    if @messages_count > 0
+      @messages = Message.where(:recipient_id => @user.id)
+    end
+  end
+
+  def outbox
+    @messages_count = Message.where(:sender_id => @user.id).count
+    if @messages_count > 0
+      @messages = Message.where(:sender_id => @user.id)
+    end
+  end
+
 end
