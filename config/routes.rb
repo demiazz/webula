df7ff20a7a8272c032ddab0ff779202b113135f4
Alexey Plutalov <demiazz.py@gmail.com>
2011-05-03 14:07:03 +0400

    Added routes for MailController. Refactoring routes.

--- config/routes.rb
+++ config/routes.rb
@@ -114,92 +114,133 @@ Webula::Application.routes.draw do
           :to => "friendship#remove_friend",
           :as => :friendship__remove_friend
 
+  #=============================================================================
+  # Mail Controller
+  #=============================================================================
+
+  #== Ящики сообщений ==========================================================
+
+  get "mail/inbox",
+          :to => "mail#inbox",
+          :as => :mail__inbox
+  
+  get "mail/outbox",
+          :to => "mail#outbox",
+          :as => :mail_outbox
+
+  #=============================================================================
   # Settings Controller
-  get "settings/avatar" => "settings#avatar_edit", :as => :settings__avatar_edit
-  put "settings/avatar" => "settings#avatar_update", :as => :settings__avatar_update
+  #=============================================================================
 
-  get "settings/profile" => "settings#profile_edit", :as => :settings__profile_edit
-  put "settings/profile" => "settings#profile_update", :as => :settings__profile_update
+  #== Редактирование профиля ===================================================
 
+  get "settings/profile", 
+          :to => "settings#profile_edit",
+          :as => :settings__profile_edit
+
+  put "settings/profile",
+          :to => "settings#profile_update",
+          :as => :settings__profile_update
+
+  #== Редактирование аватара ===================================================
+
+  get "settings/avatar",
+          :to => "settings#avatar_edit",
+          :as => :settings__avatar_edit
+
+  put "settings/avatar",
+          :to => "settings#avatar_update",
+          :as => :settings__avatar_update
+
+  #=============================================================================
   # Profiles Controller
-  get "index" => "profiles#index", :as => :profiles__index
-  get ":username" => "profiles#show", :as => :profiles__show
+  #=============================================================================
 
+  get "index",
+          :to => "profiles#index",
+          :as => :profiles__index
+
+  get ":username",
+          :to => "profiles#show",
+          :as => :profiles__show
+
+  #=============================================================================
   # ProfileMaster Controller
-  get "master/step/1" => "profile_master#main_edit", :as => :profile_master__main_edit
-  put "master/step/1" => "profile_master#main_save", :as => :profile_master__main_save
-  get "master/step/2" => "profile_master#organization_edit", :as => :profile_master__organization_edit
-  put "master/step/2" => "profile_master#organization_save", :as => :profile_master__organization_save
-  get "master/step/3" => "profile_master#contacts_edit", :as => :profile_master__contacts_edit
-  put "master/step/3" => "profile_master#contacts_save", :as => :profile_master__contacts_save
-  get "master/step/4" => "profile_master#avatar_edit", :as => :profile_master__avatar_edit
-  put "master/step/4" => "profile_master#avatar_save", :as => :profile_master__avatar_save
-  get "master/step/finish" => "profile_master#finish", :as => :profile_master__finish
+  #=============================================================================
 
+  #== Шаг 1 ====================================================================
+
+  get "master/step/1",
+          :to => "profile_master#main_edit",
+          :as => :profile_master__main_edit
+
+  put "master/step/1",
+          :to => "profile_master#main_save",
+          :as => :profile_master__main_save
+
+  #== Шаг 2 ====================================================================
+
+  get "master/step/2",
+          :to => "profile_master#organization_edit",
+          :as => :profile_master__organization_edit
+
+  put "master/step/2",
+          :to => "profile_master#organization_save",
+          :as => :profile_master__organization_save
+
+  #== Шаг 3 ====================================================================
+
+  get "master/step/3",
+          :to => "profile_master#contacts_edit",
+          :as => :profile_master__contacts_edit
+
+  put "master/step/3",
+          :to => "profile_master#contacts_save",
+          :as => :profile_master__contacts_save
+
+  #== Шаг 4 ====================================================================
+
+  get "master/step/4",
+          :to => "profile_master#avatar_edit",
+          :as => :profile_master__avatar_edit
+
+  put "master/step/4",
+          :to => "profile_master#avatar_save",
+          :as => :profile_master__avatar_save
+
+  #== Шаг 5 ====================================================================
+
+  get "master/step/finish",
+          :to => "profile_master#finish",
+          :as => :profile_master__finish
+
+  #=============================================================================
+  # Devise for Admin
+  #=============================================================================
+  
   devise_for :admins, :path => "admin/auth"
 
-  devise_for :users, :path => "auth", :controllers => { :registrations => "users/registrations" } do
-    get "settings/account" => "users/registrations#edit", :as => :settings__account_edit
-    put "settings/account" => "users/registrations#update", :as => :settings__account_update
+  #=============================================================================
+  # Devise for User
+  #=============================================================================
+
+  devise_for :users, :path => "auth", 
+                     :controllers => { :registrations => "users/registrations" } do
+
+    get "settings/account",
+            :to => "users/registrations#edit",
+            :as => :settings__account_edit
+            
+    put "settings/account",
+            :to => "users/registrations#update",
+            :as => :settings__account_update
+
   end
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-  #root :to => "devise/sessions#new"
+  #=============================================================================
+  # Root path (index)
+  #=============================================================================
 
   root :to => "profiles#index"
 
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
