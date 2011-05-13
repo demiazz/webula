1dc92e66bed737fb82defb7aa362b69c50e0f183
Alexey Plutalov <demiazz.py@gmail.com>
2011-05-13 15:35:22 +0400

    Added model CounterStat.

---
+++ app/models/counter_stat.rb
@@ -0,0 +1,19 @@
+class CounterStat
+  include Mongoid::Document
+
+  # Число запросов
+  field :count, :type => Integer, :default => ""
+
+  # Контроллер и действие
+  field :controller, :type => String, :default => ""
+  field :action, :type => String, :default => ""
+
+  # Дата (в своем формате)
+  field :year, :type => Integer, :default => 0
+  field :month, :type => Integer, :default => 0
+  field :day, :type => Integer, :default => 0
+  field :hour, :type => Integer, :default => 0
+  
+  # Ссылка на пользователя
+  belongs_to :user, :class_name => "User"
+end
